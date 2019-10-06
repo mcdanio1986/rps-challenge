b7e497894cdc5663eef608869d5a025f56db94b8
mcdanio1986 <bartosz.danek@gmail.com>
2019-10-06 19:52:37 +0100

    2nd player functioning

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -2,7 +2,7 @@ def enter_name_and_play
   visit '/'
   select 'One Player', from: 'players'
   click_button 'Submit'
-  fill_in 'name', with: "Mittens"
+  fill_in 'name', with: "Player"
   click_button 'Submit'
   select 'Rock', from: 'player_1_move'
   click_button 'Submit'
